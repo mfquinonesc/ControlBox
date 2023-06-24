a6fd898eee0f7bc1962e38d0e89d544afe5da099
manuel <manuelfqc@gmail.com>
2023-06-24 01:52:53 -0500

    [24 06 2023 01:52]

--- ControlBox/Default.aspx
+++ ControlBox/Default.aspx
@@ -1,5 +1,34 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ControlBox._Default" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-  
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server" OnLoad="UpdatePanel1_Load" UpdateMode="Conditional">
+        <ContentTemplate>
+            <div class="row">               
+                <div class="form-group">
+                    <label for="drpBoxPais">Nombre Pais</label>
+                    <asp:DropDownList ID="drpBoxPais" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="drpBoxPais_SelectedIndexChanged" />
+                </div>
+                <div class="form-group">
+                    <label for="drpBoxCiudad">Nombre Ciudad</label>
+                    <asp:DropDownList ID="drpBoxCiudad" runat="server" CssClass="form-control" AutoPostBack="true" OnSelectedIndexChanged="drpBoxCiudad_SelectedIndexChanged" OnPreRender="drpBoxCiudad_PreRender" />
+                </div>    
+            </div>
+
+            <div class="row">
+                <asp:GridView ID="GridView1" runat="server" CssClass="table table-striped" 
+                    AutoGenerateColumns="false">
+                    <Columns>                        
+                        <asp:BoundField DataField="Giro_id" HeaderText="ID" SortExpression="Giro_id" />
+                         <asp:BoundField DataField="Recibo" HeaderText="Recibo" SortExpression="Recibo" />
+                        <asp:BoundField DataField="Ciudad_id" HeaderText="Nombre Ciudad" SortExpression="Ciudad_id" />
+                    </Columns>
+                </asp:GridView>               
+            </div>
+        </ContentTemplate>
+        <Triggers>
+            <asp:PostBackTrigger ControlID="drpBoxPais" />
+            <asp:PostBackTrigger ControlID="drpBoxCiudad" />
+        </Triggers>
+    </asp:UpdatePanel>
 </asp:Content>
--- ControlBox/Default.aspx.cs
+++ ControlBox/Default.aspx.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ControlBox.Controllers;
+using ControlBox.DB;
+using Microsoft.Ajax.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +14,68 @@ namespace ControlBox
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                Paises_Controller pais_controller = new Paises_Controller();
+                pais_controller.Read().ForEach(p =>
+                {
+                    this.drpBoxPais.Items.Add(new ListItem(p.Nombre, p.Pais_id.ToString()));
+                });
+                if (drpBoxPais.Items.Count > 0)
+                {
+                    FilldrpBoxCiudad();
+                    int id = Convert.ToInt32(drpBoxCiudad.SelectedValue);
+                    LoadTable(id);
+                }
+            }
+            else
+            {
+                this.GridView1.DataSource = new List<VGiros>();
+                this.GridView1.DataBind();
+            }
+        }
 
+        protected void drpBoxPais_SelectedIndexChanged(object sender, EventArgs e)
+        {            
+            FilldrpBoxCiudad();           
+        }
+        protected void drpBoxCiudad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(drpBoxCiudad.SelectedValue);
+            LoadTable(id);
+        }
+        protected void UpdatePanel1_Load(object sender, EventArgs e)
+        {
+            UpdatePanel1.Update();
+        }
+        private void LoadTable(int id_ciudad)
+        {           
+            TCiudades ciudades = new TCiudades();
+            ciudades.Ciudad_id = id_ciudad;
+            Giros_Controller controller = new Giros_Controller();
+            List<VGiros> lst = controller.Read(ciudades);          
+            this.GridView1.DataSource = lst;
+            this.GridView1.DataBind();
+        }
+        private void FilldrpBoxCiudad()
+        {
+            this.drpBoxCiudad.Items.Clear();
+            TPaises paises = new TPaises();
+            paises.Pais_id = Convert.ToInt32(drpBoxPais.SelectedValue);
+            Ciudades_Controller ciudad_controller = new Ciudades_Controller();
+            ciudad_controller.Read(paises).ForEach(p =>
+            {
+                this.drpBoxCiudad.Items.Add(new ListItem(p.Nombre, p.Ciudad_id.ToString()));
+            });           
+        }
+        protected void drpBoxCiudad_PreRender(object sender, EventArgs e)
+        {
+            try
+            {
+                int id = Convert.ToInt32(drpBoxCiudad.SelectedValue);
+                LoadTable(id);
+            }
+            catch { }           
         }
     }
 }
